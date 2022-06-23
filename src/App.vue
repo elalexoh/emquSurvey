494779698278788834d588650ce65690df707df4
Frederick A. Gonzalez <g7.alexander@gmail.com>
2022-06-23 19:59:20 -0400

    sass install

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
+<script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      hello: "hey",
+    };
+  },
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>hey friend {{ hello }}</h1>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$variable: #bada55;
+body {
+  background-color: $variable;
 }
 </style>
