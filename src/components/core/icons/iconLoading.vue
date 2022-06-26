50ecc8fd4d03351502711adbc765bc51e2089d14
Frederick A. Gonzalez <g7.alexander@gmail.com>
2022-06-26 19:01:19 -0400

    adding icon loading

---
+++ src/components/core/icons/iconLoading.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    style="margin: auto; background: rgba(255, 255, 255, 0); display: block"
+    width="164px"
+    height="164px"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="xMidYMid"
+  >
+    <path
+      fill="none"
+      :stroke="color"
+      stroke-width="7"
+      stroke-dasharray="141.12391052246093 115.46501770019532"
+      d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z"
+      stroke-linecap="round"
+      style="transform: scale(0.8); transform-origin: 50px 50px"
+    >
+      <animate
+        attributeName="stroke-dashoffset"
+        repeatCount="indefinite"
+        dur="1s"
+        keyTimes="0;1"
+        values="0;256.58892822265625"
+      ></animate>
+    </path>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({
+  props: ["color"],
+});
+</script>
+
+<style>
+</style>
